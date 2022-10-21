0450eb6142f294458b0e2502814039c89fe86f38
Luis Pedro Morales <luismorarod98@gmail.com>
2022-10-21 11:14:37 -0600

    feat(new tournament): add multiple select typeahead for tournament's teams

--- src/pages/Tournament/Tournament1.jsx
+++ src/pages/Tournament/Tournament1.jsx
@@ -1,16 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import DatePicker from "react-datepicker";
-import Select from "react-select";
-
 import "react-datepicker/dist/react-datepicker.css";
+import Select from "react-select";
+import { Typeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
 
+// Raw data just for testing visualization.
+// MUST be changed to Redux Store approach
 const TOURNAMENT_TYPES = [
   { value: "selecciones", label: "Selecciones" },
   { value: "clubes", label: "Clubes" },
+];
+
+const TEAMS = [
+  {
+    label: "Qatar",
+  },
+  { label: "Ecuador" },
+  { label: "Senegal" },
+  { label: "Países Bajos" },
+  { label: "Inglaterra" },
+  { label: "RI de Irán" },
+  { label: "EEUU" },
+  { label: "Gales" },
+  { label: "Argentina" },
+  { label: "Arabia Saudí" },
+  { label: "México" },
+  { label: "Polonia" },
+  { label: "Francia" },
+  { label: "Dinamarca" },
+  { label: "Túnez" },
+  { label: "Australia" },
+  { label: "España" },
+  { label: "Alemania" },
+  { label: "Japón" },
+  { label: "Costa Rica" },
+  { label: "Bélgica" },
+  { label: "Canadá" },
+  { label: "Marruecos" },
+  { label: "Croacia" },
+  { label: "Brasil" },
+  { label: "Serbia" },
+  { label: "Suiza" },
+  { label: "Camerún" },
+  { label: "Portugal" },
+  { label: "Ghana" },
+  { label: "Uruguay" },
+  { label: "República de Corea" },
 ];
 
 /**
@@ -22,6 +62,8 @@ const TOURNAMENT_TYPES = [
  * @returns
  */
 const Tournament1 = ({ tournament, updateTournament, nextStep }) => {
+  const typeaheadRef = useRef(null);
+
   return (
     <div className="centered">
       <h3 className="mb-5 fw-light">Crear nuevo torneo</h3>
@@ -54,7 +96,7 @@ const Tournament1 = ({ tournament, updateTournament, nextStep }) => {
                   onChange={(date) => {
                     updateTournament({ startDate: date });
                   }}
-                  placeholderText="Select a date..."
+                  placeholderText="Seleccione una fecha..."
                 />
               </Form.Group>
             </Col>
@@ -67,7 +109,7 @@ const Tournament1 = ({ tournament, updateTournament, nextStep }) => {
                     updateTournament({ endDate: date });
                   }}
                   minDate={tournament.startDate}
-                  placeholderText="Select a date..."
+                  placeholderText="Seleccione una fecha..."
                 />
               </Form.Group>
             </Col>
@@ -92,6 +134,19 @@ const Tournament1 = ({ tournament, updateTournament, nextStep }) => {
         <Container>
           <Form.Group className="mb-3" controlId="tournamentType">
             <Form.Label>Equipos participantes</Form.Label>
+            <Typeahead
+              multiple
+              id="keep-menu-open"
+              onChange={(selected) => {
+                updateTournament({ teams: selected });
+                // Keep the menu open when making multiple selections.
+                typeaheadRef.current.toggleMenu();
+              }}
+              options={TEAMS}
+              placeholder="Escoja los equipos..."
+              ref={typeaheadRef}
+              selected={tournament.teams}
+            />
           </Form.Group>
         </Container>
       </Form>
