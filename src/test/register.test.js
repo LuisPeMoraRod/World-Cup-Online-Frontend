f01c6423d1688cfa24b1377ffcdee560bc2c96a5
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2022-11-28 19:39:27 -0600

    test(forms): test the privateLeague, the LogIn and the Register page

--- src/test/register.test.js
+++ src/test/register.test.js
@@ -7,94 +7,55 @@ import store from "../store/store";
 import Register from '../pages/Register/Register'
 import {BrowserRouter as Router} from 'react-router-dom';
 
-test('Contains the "Registro World Cup Online" title',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Registro World Cup Online')
+
+describe('Testing <Login/> page', () => {
+    let component
+    beforeEach(() => {
+        component = render(
+            <Provider store={store}>
+                <Router>
+                    <Register/>
+                </Router>
+            </Provider>)
+    })
+
+    test('Contains the "Registro World Cup Online" title',  () => {
+        expect(component.getByText('Registro World Cup Online'))
+    })
+    
+    test('Contains the "Nombre de usuario:" label',  () => {
+        expect(component.getByText('Nombre de usuario:'))
+    })
+    
+    test('Contains the "Nombre:" label',  () => {
+        expect(component.getByText('Nombre:'))
+    })
+        
+    test('Contains the "Apellido:" label',  () => {
+        expect(component.getByText('Apellido:'))
+    })
+    
+    test('Contains the "Correo Electrónico:" label',  () => {
+        expect(component.getByText('Correo Electrónico:'))
+    })
+    
+    test('Contains the "Contraseña:" label',  () => {
+        expect(component.getByText('Contraseña:'))
+    })
+    
+    test('Contains the "Fecha de nacimiento:" label',  () => {
+        expect(component.getByText('Fecha de nacimiento:'))
+    })
+    
+    test('Contains the "País de procedencia:" label',  () => {
+        expect(component.getByText('País de procedencia:'))
+    })
+    
+    test('Contains the "términos y condiciones" option',  () => {
+        expect(component.getByText('términos y condiciones'))
+    })
+
+
 })
 
-test('Contains the "Nombre de usuario:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Nombre de usuario:')
-})
 
-test('Contains the "Nombre:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Nombre:')
-})
-
-
-
-test('Contains the "Apellido:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Apellido:')
-})
-
-test('Contains the "Correo Electrónico:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Correo Electrónico:')
-})
-
-test('Contains the "Contraseña:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Contraseña:')
-})
-
-test('Contains the "Fecha de nacimiento:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('Fecha de nacimiento:')
-})
-
-test('Contains the "País de procedencia:" label',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('País de procedencia:')
-})
-
-test('Contains the "términos y condiciones" option',  () => {
-    const component = render(
-                            <Provider store={store}>
-                                <Router>
-                                    <Register/>
-                                </Router>
-                            </Provider>)
-    component.getByText('términos y condiciones')
-})
